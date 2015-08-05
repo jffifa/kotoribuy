ea2f5961024220ceb8717dec9abdd3df78da4512
jffifa <jffifa@gmail.com>
2015-08-06 02:20:59 +0800

    modified

--- src/expo/models.py
+++ src/expo/models.py
@@ -18,7 +18,7 @@ class Booth(models.Model):
 
     event = models.ForeignKey(Event, verbose_name=u'展会')
     circle = models.CharField(max_length=64, verbose_name=u'社团名/企业名')
-    logo = models.ImageField(blank=True, max_length=255, verbose_name=u'LOGO')
+    logo = models.ImageField(upload_to='booth-logo', blank=True, max_length=255, verbose_name=u'LOGO')
     location = models.CharField(max_length=64, blank=True, verbose_name=u'摊位号')
     page = models.URLField(max_length=255, blank=True, verbose_name=u'专门页面')
     tags = models.TextField(blank=True, verbose_name=u'标签（用英文逗号,分隔）')
