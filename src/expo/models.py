0917fce693e75b9ef6940a27d6803e45256554d6
jffifa <jffifa@gmail.com>
2015-08-06 02:02:42 +0800

    modified

--- src/expo/models.py
+++ src/expo/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.db import models
 
 # Create your models here.
@@ -15,4 +16,10 @@ class Booth(models.Model):
         verbose_name = u'摊位'
         verbose_name_plural = u'摊位'
 
-    circle = models.CharField(max_length=64, verbose_name=u'')
+    event = models.ForeignKey(Event, verbose_name=u'展会')
+    circle = models.CharField(max_length=64, verbose_name=u'社团名/企业名')
+    logo = models.ImageField(blank=True, max_length=255, verbose_name=u'LOGO')
+    location = models.CharField(max_length=64, blank=True, verbose_name=u'摊位号')
+    page = models.URLField(max_length=255, blank=True, verbose_name=u'专门页面')
+    tags = models.TextField(blank=True, verbose_name=u'标签（用英文逗号,分隔）')
+    comment = models.TextField(blank=True, verbose_name=u'备注')
