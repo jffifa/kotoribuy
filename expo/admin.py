1d2a5cd1d812ef8c840f4ac605f7e47e51be11d1
jffifa <jffifa@gmail.com>
2015-08-09 01:08:52 +0800

    modified

--- expo/admin.py
+++ expo/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Event, Booth
+from .models import Event, Booth, Tag
 
 admin.site.register(Event)
 admin.site.register(Booth)
+admin.site.register(Tag)
