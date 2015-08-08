8c7a1a0cd1b9283aab45df6884a95853b72dd018
jffifa <jffifa@gmail.com>
2015-08-09 07:05:59 +0800

    modified

--- expo/admin.py
+++ expo/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 
+import autocomplete_light
 from .models import Event, Booth, Tag
 
+
+class BoothTagInline(admin.StackedInline):
+    form = autocomplete_light.modelform_factory(Tag)
+    model = Tag
+    extra = 0
+
+
+class BoothAdmin(admin.ModelAdmin):
+    form = autocomplete_light.modelform_factory(Booth)
+
+    inlines = [BoothTagInline,]
+
+    search_fields = ['circle', 'location', 'tag']
+
 admin.site.register(Event)
-admin.site.register(Booth)
+admin.site.register(Booth, BoothAdmin)
 admin.site.register(Tag)
