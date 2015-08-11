679341eb89c2b0c28ca58e6b94682a0053ce5e7f
jffifa <jffifa@gmail.com>
2015-08-11 23:47:12 +0800

    modified

--- expo/urls.py
+++ expo/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^index$', views.index, name='index'),
     url(r'^tag/(?P<tag_id>[0-9]+)$', views.tag_filter, name='tag_id'),
     url(r'^tag/search/(?P<tag_query>.+)$', views.tag_filter, name='tag_query'),
